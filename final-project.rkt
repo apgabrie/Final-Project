3c7c22fb023ff1d775f47b3826994fc71c171f5b
Andrew P Gabriel <andrew_gabriel@student.uml.edu>
2015-04-26 19:11:34 -0400

    Added experience drops when enemies are killed. Added different behaviors for first weapon when at different experience level. Continued impplementation of pause menu.

--- final-project.rkt
+++ final-project.rkt
@@ -33,16 +33,23 @@
 (define get-exp eighth)
 (define get-inventory-sprites ninth)
 (define get-wait-counter tenth)
+(define (get-weapon y)
+  (caddr (cddddr (cddddr y))))
 
 (define (change-world-sprites y sprites)
-  (make-world (get-state y) (get-map y) sprites (get-bg y) (get-map-offset y) (get-tiles-on-left y) (get-health y) (get-exp y) (get-inventory-sprites y) (get-wait-counter y)))
+  (make-world (get-state y) (get-map y) sprites (get-bg y) (get-map-offset y) (get-tiles-on-left y) 
+              (get-health y) (get-exp y) (get-inventory-sprites y) (get-wait-counter y) (get-weapon y)))
 (define (change-world-inventory-sprites y sprites)
-  (make-world (get-state y) (get-map y) (get-sprites y) (get-bg y) (get-map-offset y) (get-tiles-on-left y) (get-health y) (get-exp y) sprites (get-wait-counter y)))
+  (make-world (get-state y) (get-map y) (get-sprites y) (get-bg y) (get-map-offset y) (get-tiles-on-left y) 
+              (get-health y) (get-exp y) sprites (get-wait-counter y) (get-weapon y)))
+(define (change-world-wait-counter y count)
+  (make-world (get-state y) (get-map y) (get-sprites y) (get-bg y) (get-map-offset y) (get-tiles-on-left y) 
+              (get-health y) (get-exp y) (get-inventory-sprites y) count (get-weapon y)))
 
 (define (main y)
   (big-bang y
             [on-tick update]
-            [stop-when (lambda (y) (= (get-map y) 3))]
+            [stop-when (lambda (y) (= (get-map y) 5))]
             [to-draw draw]
             [on-key keys-down]
             [on-release key-release]))
@@ -89,27 +96,37 @@
 (define (update y)
   (cond ; NEXT STAGE WAIT
         ((and (eq? (get-state y) "next stage wait") (> (get-wait-counter y) 0))
-         (make-world (get-state y) (get-map y) (get-sprites y) (get-bg y) (get-map-offset y) (get-tiles-on-left y) (get-health y) (get-exp y) (get-inventory-sprites y) (- (get-wait-counter y) 1)))
+         (make-world (get-state y) (get-map y) (get-sprites y) (get-bg y) (get-map-offset y) (get-tiles-on-left y) 
+                     (get-health y) (get-exp y) (get-inventory-sprites y) (- (get-wait-counter y) 1) (get-weapon y)))
         ((and (eq? (get-state y) "next stage wait") (<= (get-wait-counter y) 0))
-         (set! current-map map-two)
-         (make-world "playing" (+ (get-map y) 1) sprite-list-three (get-bg y) 0 0 (get-health y) (get-exp y) (get-inventory-sprites y) 0))
+         (set! current-map (map-by-number (+ (get-map y) 1)))
+         (make-world "playing" (+ (get-map y) 1) (sprite-list-by-map-number (+ (get-map y) 1)) (get-bg y) 0 0 
+                     (get-health y) (get-exp y) (get-inventory-sprites y) 0 (get-weapon y)))
         ; PAUSE BUTTON PRESS
         ((and shift-button (eq? (get-state y) "playing"))
          (set! shift-button #f)
-         (make-world "pause menu" (get-map y) (get-sprites y) pause-menu (get-map-offset y) (get-tiles-on-left y) (get-health y) (get-exp y) (get-inventory-sprites y) (get-wait-counter y)))
+         (make-world "pause menu" (get-map y) (get-sprites y) pause-menu (get-map-offset y) (get-tiles-on-left y) 
+                     (get-health y) (get-exp y) (get-inventory-sprites y) (get-wait-counter y) (get-weapon y)))
         ((and shift-button (eq? (get-state y) "pause menu"))
          (set! shift-button #f)
-         (make-world "playing" (get-map y) (get-sprites y) bg-1 (get-map-offset y) (get-tiles-on-left y) (get-health y) (get-exp y) (get-inventory-sprites y) (get-wait-counter y)))
-        ; PAUSE MENU
+         (make-world "playing" (get-map y) (get-sprites y) bg-1 (get-map-offset y) (get-tiles-on-left y) 
+                     (get-health y) (get-exp y) (get-inventory-sprites y) (get-wait-counter y) (get-weapon y)))
+        ; PAUSE MENU UPDATE
         ((eq? (get-state y) "pause menu")
          (let* ((new-sprites (map (lambda (sprite) ((sprite-update sprite) sprite))(get-inventory-sprites y)))
                (cursor (filter cursor? new-sprites)))
-           (cond ((and z-button (is-state? (car cursor) "save position"))
+           (cond ((> (get-wait-counter y) 0)
+                  (change-world-wait-counter y (- (get-wait-counter y) 1)))
+                 ((and z-button (is-state? (car cursor) "save position"))
                   (set! z-button #f)
                   (save-world y)
-                  y)
+                  (change-world-wait-counter y 40))
+                 ((and z-button (is-state? (car cursor) "back position"))
+                  (set! z-button #f)
+                  (make-world "playing" (get-map y) (get-sprites y) bg-1 (get-map-offset y) (get-tiles-on-left y) 
+                              (get-health y) (get-exp y) (get-inventory-sprites y) (get-wait-counter y) (get-weapon y)))
                  (else (change-world-inventory-sprites y new-sprites)))))
-        ; PLAYING
+        ; PLAYING UPDATE
         ((eq? (get-state y) "playing")
          (let* ((new-sprites (foldr (lambda (x y) (cond ((eq? (car x) "kill-me") y)
                                                         ((eq? (car x) "pop-me") (append (cdr x) y))
@@ -122,11 +139,22 @@
                                            (get-sprites y))))
                 (player (car (filter player? new-sprites)))
                 (enemies-colliding-w-player (enemy-collision new-sprites))
-                (damage (if (or (is-state? player "get hurt") (null? enemies-colliding-w-player)) 0 (sprite-damage (car enemies-colliding-w-player))))
+                (damage (cond ((is-state? player "place me") (null? enemies-colliding-w-player)
+                               30)
+                              ((or (is-state? player "get hurt") (null? enemies-colliding-w-player)) 
+                               0)
+                              (else (sprite-damage (car enemies-colliding-w-player)))))
                 (player-maybe-hurt (if (null? enemies-colliding-w-player) 
                                        player 
                                        (change-sprite-velY (change-sprite-frame-counter (change-sprite-state player "get hurt") 100) 10)))
-                (new-player (cond ((is-state? player-maybe-hurt "get hurt") player-maybe-hurt)
+                (new-player (cond ((is-state? player-maybe-hurt "place me")
+                                   (let ((check-point (find-checkpoint current-map (sprite-gridX player))))
+                                     (change-sprite-state
+                                      (change-sprite-coords player 
+                                                            (* 64 (car check-point))
+                                                            (* 64 (cadr check-point)))
+                                      "stand")))
+                                  ((is-state? player-maybe-hurt "get hurt") player-maybe-hurt)
                                   ((and (not (is-state? player "jump")) (left-key-not-right-key)) (change-sprite-state (change-sprite-direction player "left") "walk"))
                                   ((and (not (is-state? player "jump")) (right-key-not-left-key)) (change-sprite-state (change-sprite-direction player "right") "walk"))
                                   ((not (is-state? player "jump")) (change-sprite-state player "stand"))
@@ -134,7 +162,7 @@
                 
                 ; prevents projectiles being shoot too close together
                 (new-wait-timer (cond ((and x-button (= (get-wait-counter y) 0))
-                                       2)
+                                       3)
                                       ((> (get-wait-counter y) 0)
                                        (- (get-wait-counter y) 1))
                                       (else 0)))
@@ -143,42 +171,49 @@
                 (projectiles (filter projectile? new-sprites))
                 (projectiles-not-touching-enemies (filter (lambda (x) (not (sprite-collides-over-list? x enemies))) projectiles))
                 (new-projectiles (if (and x-button (= (get-wait-counter y) 0))
-                                     (append projectiles-not-touching-enemies (shoot-weapon player))
+                                     (append projectiles-not-touching-enemies (shoot-weapon player (get-exp y) (get-weapon y)))
                                      projectiles-not-touching-enemies))
                 
                 (enemies-after-projectiles (if (null? projectiles)
                                                enemies
                                                (projectile-collisions projectiles enemies)))
                 (items (filter item? new-sprites))
+                (new-items (filter (lambda (x) (not (sprites-collide? x player))) items))
+                (new-exp (foldr (lambda (x y) (+ (sprite-damage x) y)) 0 (filter (lambda (x) (sprites-collide? x player)) items)))
+                
                 (maybe-next-stage (filter (lambda (x) (sprites-collide? player x)) new-sprites))
                 
                 (side-scroll-values (side-scroll player (get-map-offset y) (get-tiles-on-left y)))
                 (new-map-offset (first side-scroll-values))
                 (new-tiles-on-left (second side-scroll-values)))
            (if (member "item-next-stage" maybe-next-stage (lambda (x y) (is-type? x y)))
-               (make-world "next stage wait" (get-map y) new-sprites (get-bg y) new-map-offset new-tiles-on-left (get-health y) (get-exp y) (get-inventory-sprites y) 65)
+               (make-world "next stage wait" (get-map y) new-sprites (get-bg y) new-map-offset new-tiles-on-left 
+                           (get-health y) (get-exp y) (get-inventory-sprites y) 65 (get-weapon y))
                (make-world (get-state y) (get-map y) 
                            ; new sprite-list
-                           (append enemies-after-projectiles new-projectiles (list new-player) items)
-                           (get-bg y) new-map-offset new-tiles-on-left (- (get-health y) damage) (get-exp y) (get-inventory-sprites y) new-wait-timer))))
-        ; TITLE SCREEN
+                           (append enemies-after-projectiles new-projectiles (list new-player) new-items)
+                           (get-bg y) new-map-offset new-tiles-on-left (- (get-health y) damage) 
+                           (+ (get-exp y) new-exp) (get-inventory-sprites y) new-wait-timer (get-weapon y)))))
+        ; TITLE SCREEN UPDATE
         ((eq? (get-state y) "title screen")
          (let* ((new-sprites (map (lambda (sprite) ((sprite-update sprite) sprite)) (get-sprites y)))
                (cursor (filter cursor? new-sprites)))
            (cond ((and z-button (is-state? (car cursor) "start"))
                   (set! z-button #f)
-                  (make-world "playing" 1 sprite-list-two bg-1 (get-map-offset y) (get-tiles-on-left y) (get-health y) (get-exp y) (get-inventory-sprites y) (get-wait-counter y)))
+                  (make-world "playing" 1 sprite-list-one bg-1 (get-map-offset y) (get-tiles-on-left y) 
+                              (get-health y) (get-exp y) (get-inventory-sprites y) (get-wait-counter y) (get-weapon y)))
                  ((and z-button (is-state? (car cursor) "continue"))
                   (set! z-button #f)
                   ;(make-world-from-save)
                   ;(continue)
-                  (make-world "playing" (first continue) sprite-list-two bg-1 (get-map-offset y) (get-tiles-on-left y) (second continue) (third continue) (get-inventory-sprites y) (get-wait-counter y)))
+                  (make-world "playing" (first continue) (sprite-list-by-map-number (first continue)) bg-1 (get-map-offset y) (get-tiles-on-left y) 
+                              (second continue) (third continue) (get-inventory-sprites y) (get-wait-counter y) (get-weapon y)))
                  (else (change-world-sprites y new-sprites)))))
         (else y)))
 
 
 (define (draw y)
-  (cond ; NEXT STAGE WAIT
+  (cond ; NEXT STAGE WAIT DRAW
         ((eq? (get-state y) "next stage wait")
          (let ((health-width (if (> (get-health y) 0) (get-health y) 0)))
            ; same as in playing but also go to next stage picture
@@ -186,22 +221,34 @@
                               120 50 'left 'top
                               (draw-sprites (get-sprites y)
                                             (draw-map (choose-map (get-map y) (get-tiles-on-left y)) (get-bg y) (get-map-offset y)) (+ (get-map-offset y) (* 64 (get-tiles-on-left y)))))))
-        ; PLAYING
+        ; PLAYING DRAW
         ((eq? (get-state y) "playing")
-         (let ((health-width (if (> (get-health y) 0) (get-health y) 0)))
+         (let ((health-width (if (> (get-health y) 0) (get-health y) 0))
+               (weapon-image (cond ((= (get-weapon y) 1)
+                                    bubble-spell)
+                                   (else (empty-scene 0 0)))))
            ; draw background, then draw map into the background, then draw sprites into map, then draw HUD onto the whole thing
-           (place-image/align (place-image/align (place-image/align expbar 0 0 'left 'top (empty-scene (* 1.37 (- 100 health-width)) 26))
-                                                 103 50 'left 'top
-                                                 (place-image/align (place-image/align healthbar 0 0 'left 'top (empty-scene (* 1.37 health-width) 26))
-                                                                    103 16 'left 'top
-                                                                    HUD))
-                              10 10 'left 'top
-                              (draw-sprites (get-sprites y)
-                                            (draw-map (choose-map (get-map y) (get-tiles-on-left y)) (get-bg y) (get-map-offset y)) (+ (get-map-offset y) (* 64 (get-tiles-on-left y)))))))
-        ; PAUSE MENU
+           (place-image/align 
+            weapon-image 32 32 'left 'top
+            (place-image/align (make-HUD y health-width)
+                               10 10 'left 'top
+                               (draw-sprites (get-sprites y)
+                                             (draw-map (choose-map (get-map y) (get-tiles-on-left y)) (get-bg y) (get-map-offset y)) (+ (get-map-offset y) (* 64 (get-tiles-on-left y))))))))
+        ; PAUSE MENU DRAW
         ((eq? (get-state y) "pause menu")
-         (place-image/align (text "A basic spell. Shoots a bubble to hurt enemies." 24 'white) 50 365 'left 'top 
-                            (draw-sprites (get-inventory-sprites y) pause-menu 0)))
+         (let* ((cursor (car (filter cursor? (get-inventory-sprites y))))
+                (spells (filter spell? (get-inventory-sprites y)))
+                (items (filter (lambda (x) (not (or (spell? x) (cursor? x)))) (get-inventory-sprites y)))
+                (name (item-name-from-cursor-state (sprite-state cursor) spells items))
+                (item-text (item-text-from-cursor-state (sprite-state cursor) spells items))
+                (game-saved-message (if (> (get-wait-counter y) 0)
+                                        game-saved
+                                        (empty-scene 0 0))))
+           (place-image/align game-saved-message  105 167 'left 'top
+                              (place-image/align equip-image (+ (* 139 (- (get-weapon y) 1)) 131) 157 'left 'top
+                                                 (place-image/align (text name 20 'black) 85 360 'left 'top 
+                                                                    (place-image/align (text item-text 20 'black) 60 390 'left 'top 
+                                                                                       (draw-sprites (get-inventory-sprites y) pause-menu 0)))))))
         ; else case is Title Screen
         (else (draw-sprites (get-sprites y) (get-bg y) 0))))
 
@@ -214,6 +261,17 @@
 (define expbar (bitmap "images/expbar.png"))
 (define HUD (bitmap "images/HUD.png"))
 (define next-stage-picture (bitmap "images/next-stage.png"))
+(define game-saved (bitmap "images/menu/game-saved.png"))
+
+(define (make-HUD y health-width)
+  (place-image/align 
+   (text (number->string (floor (/ (get-exp y) 60))) 17 (color 57 0 29 255))
+   87 52 'left 'top
+   (place-image/align (place-image/align expbar 0 0 'left 'top (empty-scene (* 1.72 (modulo (get-exp y) 60)) 24))
+                                        115 49 'left 'top
+                                        (place-image/align (place-image/align healthbar 0 0 'left 'top (empty-scene (* 1.03 health-width) 24))
+                                                           115 16 'left 'top
+                                                           HUD))))
 
 (define (save-world y)
   (let ((map    (get-map y))
@@ -225,6 +283,6 @@
 (define continue
     (map car (read-words-and-numbers/line "save.txt")))
 
-(main (make-world "title screen" 0 sprite-list-one title-screen-bg 0 0 100 0 (list menu-cursor-pos-1) 0))
+(main (make-world "title screen" 0 title-sprite-list title-screen-bg 0 0 100 0 inventory-list-one 0 1))
 
 
